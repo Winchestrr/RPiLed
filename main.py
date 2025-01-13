7abc3dff598dd59ce6c429b7e83fa0f972e640b4
Jakub Nowak <jakub_nowak@interia.pl>
2025-01-13 21:06:58 +0100

    pierwszy test pca

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+from time import sleep
+from adafruit_pca9685 import PCA9685
+import board
+import busio
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Ustawienie I2C
+i2c = busio.I2C(board.SCL, board.SDA)
 
+# Inicjalizacja PCA9685
+pca = PCA9685(i2c)
+pca.frequency = 1000  # Częstotliwość PWM
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Piny dla LED-ów
+led1_pin = 0
+#led2_pin = 1
 
+# Funkcja do płynnego zapalania/gaszenia
+def fade_led(pin, duration, steps):
+    for i in range(steps):
+        duty_cycle = int((i / steps) * 0xFFFF)
+        pca.channels[pin].duty_cycle = duty_cycle
+        sleep(duration / (2 * steps))
+    #for i in range(steps, 0, -1):
+     #   duty_cycle = int((i / steps) * 0xFFFF)
+      #  pca.channels[pin].duty_cycle = duty_cycle
+       # sleep(duration / (2 * steps))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    while True:
+        fade_led(led1_pin, duration=1, steps=50)  # Szybki fade in/out
+        # fade_led(led2_pin, duration=2, steps=50)  # Wolniejszy fade in/out
+finally:
+    pca.deinit()
