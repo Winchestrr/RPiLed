a82fc498badab939bbc50eb9338fe0554982675f
Jakub Nowak <jakub_nowak@interia.pl>
2025-01-14 21:39:04 +0100

    test4

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
+# Ustawienie numeracji pinów GPIO na fizyczne numery pinów
+GPIO.setmode(GPIO.BOARD)
 
-while True:
-    GPIO.output(17,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(17,GPIO.LOW)
-    time.sleep(1)
+# Definicja pinów, do których podłączone są diody
+LED1 = 17
+LED2 = 18
+
+# Ustawienie pinów jako wyjścia
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+
+# Funkcja do mrugania diodą
+def mrugaj(pin, czas):
+    while True:
+        GPIO.output(pin, GPIO.HIGH)  # Zapal dioda
+        time.sleep(czas)
+        GPIO.output(pin, GPIO.LOW)   # Zgaś dioda
+        time.sleep(czas)
+
+# Utworzenie dwóch wątków do mrugania diodami
+try:
+    import threading
+    thread1 = threading.Thread(target=mrugaj, args=(LED1, 1))
+    thread2 = threading.Thread(target=mrugaj, args=(LED2, 0.3))
+    thread1.start()
+    thread2.start()
+
+    while True:
+        pass
+
+except KeyboardInterrupt:
+    # Czyszczenie stanu pinów GPIO przy przerwaniu
+    GPIO.cleanup()
