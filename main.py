da589d3e0d84d82babdbc3cc94c7c1fddd4a3241
Jakub Nowak <jakub_nowak@interia.pl>
2025-01-14 21:32:23 +0100

    test3

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
-GPIO.output(17,GPIO.HIGH)
-time.sleep(10)
-GPIO.output(17,GPIO.LOW)
+
+while True:
+    GPIO.output(17,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(17,GPIO.LOW)
+    time.sleep(1)
